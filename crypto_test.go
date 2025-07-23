bf211010fde9784aabd0f8e12a356029771f2274
Вадим <vadim@MBP-Vadim.NEOTECH>
2025-07-23 17:32:53 +0500

    egegkglrggkeg

---
+++ crypto_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestCopyEncryptDecrypt(t *testing.T) {
+	src := bytes.NewReader([]byte("Foo not Bar"))
+	dst := new(bytes.Buffer)
+	key := newEncryptionKey()
+	_, err := copyEncrypt(key, src, dst)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(dst.String())
+
+	out := new(bytes.Buffer)
+	if _, err := copyDecrypt(key, dst, out); err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(out.String())
+}
